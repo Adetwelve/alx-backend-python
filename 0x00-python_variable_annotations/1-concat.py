8831c44f36d04411b6bba62c10e9280d6c2c6b59
Adetwelve <niyero10@gmail.com>
2023-07-07 00:03:02 +0000

    task 1:concat two string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function: concat string
+    parametar: str1, str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns a concatenated sting """
+    return str1 + str2
