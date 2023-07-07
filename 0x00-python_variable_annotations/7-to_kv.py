3d3544076f1b86239e99f0681859fc269de69d29
Adetwelve <niyero10@gmail.com>
2023-07-07 03:02:22 +0000

    task 7: fix tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,6 +5,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ return a tuple containing key and the square of the value """
     return k, v ** 2
