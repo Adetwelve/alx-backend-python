dce2407363cbba83ba7b633643069dfa68ea5e09
Adetwelve <niyero10@gmail.com>
2023-07-07 02:24:30 +0000

    task 6: fix style

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """ type-annotated function: sum_mixed_list """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
