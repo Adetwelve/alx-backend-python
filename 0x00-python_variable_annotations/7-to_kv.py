74d59821ff278ca10aa592090fb060732e6b95db
Adetwelve <niyero10@gmail.com>
2023-07-07 02:57:59 +0000

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" type-annotated function: to_kv
+    parameter: k, v
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+    """ return a tuple containing key and the square of the value """
+    return k, v ** 2
