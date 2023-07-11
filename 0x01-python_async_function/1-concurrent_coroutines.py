9a0a3970d93efda0dbefce067a1a82c22ecb21c8
Adetwelve <niyero10@gmail.com>
2023-07-11 00:18:08 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" An async function execute multiple 
+""" An async function execute multiple
     coroutines at the same time with async
 """
 import asyncio
 import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ A couroutine that return random number n times """
