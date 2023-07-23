a972c3bbe355450d0ba0ac5c016a7d87b32b44e5
Adetwelve <niyero10@gmail.com>
2023-07-23 11:47:55 +0000

    task 101:

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,8 +8,9 @@ T = TypeVar('T')
 Resp = Union[Any, T]
 Deft = Union[T, None]
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Deft = None) -> Resp:
-    """ 
+    """
        A funtion that retrieves value from a dict using key.
     """
     if key in dct:
