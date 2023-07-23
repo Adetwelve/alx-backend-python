52bac6764d1f29216b445226b19cd837c247a853
Adetwelve <niyero10@gmail.com>
2023-07-23 11:43:44 +0000

    Task 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" A type_annotated function """
+
+
+from typing import Mapping, Any, Optional, TypeVar, Union
+
+T = TypeVar('T')
+Resp = Union[Any, T]
+Deft = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: Deft = None) -> Resp:
+    """ 
+       A funtion that retrieves value from a dict using key.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
