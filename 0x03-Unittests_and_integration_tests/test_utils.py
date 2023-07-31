68a37e563648ebc3807eb8d9dc43b0935d84447c
Adetwelve <niyero10@gmail.com>
2023-07-31 18:54:15 +0100

    task 1: raise an exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Parameterize a unit test """
 import unittest
-from typing import Dict, Tuple, Union
+from typing import Tuple, Union, Dict, Any, Type
 from utils import access_nested_map
 from parameterized import parameterized
 
@@ -22,3 +22,21 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         """ Test 'access_nested_map' output """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+
+
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict[Any, Any],
+            path: Tuple[str],
+            expected: Type[Exception]
+            ) -> None:
+        """ Test "access_nested_map" for Keyerror """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+
+
